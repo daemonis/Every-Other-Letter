83f82c01c1a26d851e35076ba36a5c809d088610
Melanie Zeller <melanie.zeller@verizon.net>
2022-02-10 21:24:13 -0800

    Add project files.

---
+++ ConsoleApp2/EveryOtherLetter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace EveryOtherLetter
+{
+    class EveryOtherLetter
+    {
+        private static string? body;
+        private static int lengthOfString;
+
+        static void GetString()
+        {
+            body = Console.ReadLine();
+            lengthOfString = body.Length;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word or phrase to make every other letter uppercase: ");
+            int i = 0;
+
+            do
+            {
+                GetString();
+                if (body == null || lengthOfString <= 0)
+                {
+                    Console.Write("Please enter a word or phrase: ");
+                    continue;
+                }
+            }
+            while (body == null || lengthOfString <= 0);
+
+                Console.WriteLine($"Every other letter, starting with {body[0]}, capitalized: \n");
+            do
+            {
+                string startOfString = Convert.ToString(body[i]);
+                if (i % 2 == 0)
+                {
+                    startOfString = startOfString.ToUpper();
+                }
+                Console.Write(startOfString);
+                i++;
+            }
+            while (i < lengthOfString);
+        }
+    }
+}
